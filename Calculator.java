c31bb164a65e34e57bcdb1220a1e03f7a0fceedd
ADKAR DNYANESH RAMDAS <dnyaneshadkar@gmail.com>
2025-12-17 21:51:00 +0530

    The Arithmetic Operators

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+
+public class Calculator {
+	
+	public static void main(String[] args) {
+		
+		// Here we will perform mathematical operations
+		int a = 8;
+		int b = 4;
+		System.out.println("-->Given a = " + a + " and b = " + b);
+		System.out.println(" ");
+		
+		
+		System.out.println("-->Addition of a and b is: " + (a+b));
+		System.out.println(" ");
+		
+		
+		System.out.println("-->Substraction of a and b is: " + (a-b));
+		System.out.println(" ");
+		
+		
+		System.out.println("-->Multiplication of a and b is: " + (a*b));
+		System.out.println(" ");
+		
+		
+		System.out.println("-->Division of a and b is: " + (a/b));
+		System.out.println(" ");
+		
+		System.out.println("-->Modulus of a and b is: " + (a%b));
+		
+	}
+
+}
